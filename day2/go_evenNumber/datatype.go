2c16aa42afd37913365828f607640c2482d5abec
farhanmiqbal <farhanmiqbal10@gmail.com>
2025-01-27 10:46:41 +0700

    update datatype

--- day2/go_evenNumber/datatype.go
+++ day2/go_evenNumber/datatype.go
@@ -17,7 +17,20 @@ func main(){
 	var tinggi float64 = 170.5
 	var aktif bool = true
 
-	
+
+	fmt.Println("nama :", nama)
+	fmt.Println("umur :", umur)
+	fmt.Println("tinggi :", tinggi)
+	fmt.Println("status aktif :", aktif)
+
+	panjang := 10.5
+	lebar := 5.2
+	luas := luasPersegiPanjang(lebar, panjang)
+	fmt.Println("luas persegi panjang :", luas)
+
+	fmt.Println(sapa)
+
+
 	
 
 }
