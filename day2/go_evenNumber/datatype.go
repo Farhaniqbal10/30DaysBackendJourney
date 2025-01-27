d7649a9d1b766fd12480e786f52e49d851b08525
farhanmiqbal <farhanmiqbal10@gmail.com>
2025-01-27 10:41:31 +0700

    create day2

---
+++ day2/go_evenNumber/datatype.go
@@ -0,0 +1,23 @@
+package main 
+
+import "fmt"
+
+func luasPersegiPanjang(lebar float64, panjang float64){
+	return lebar*panjang
+}
+
+func sapa(nama string) string {
+	return "halo" + nama + "selamat datang di program go"
+}
+
+func main(){
+
+	var nama string = "farhan"
+	umur := 23
+	var tinggi float64 = 170.5
+	var aktif bool = true
+
+	
+	
+
+}
