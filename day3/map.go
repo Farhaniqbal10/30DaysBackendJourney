2111073bfb934d4011c3753788ebfaca13563a0d
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-30 09:49:55 +0700

    Update map.go

--- day3/map.go
+++ day3/map.go
@@ -8,4 +8,12 @@ func main(){
   mahasiswa["1001"] = "Farhan"
   mahasiswa["1002"] = "Maulana"
   mahasiswa["1003"] = "Iqbal"
+
+  fmt.Println(mahasiswa)
+
+  fmt.Println(mahasiswa["1001"])
+
+  for nim, nama := range mahasiswa {
+  fmt.Println(a...: "Nim", nim, "bernama", nama)  
+  }
 }
