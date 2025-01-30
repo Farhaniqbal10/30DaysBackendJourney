d21e94ab85fb998255b9d071f38c490f099110ed
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-30 09:46:59 +0700

    Create map.go

---
+++ day3/map.go
@@ -0,0 +1,11 @@
+package main 
+
+import "fmt"
+
+func main(){
+  mahasiswa := make(map[string]string)
+
+  mahasiswa["1001"] = "Farhan"
+  mahasiswa["1002"] = "Maulana"
+  mahasiswa["1003"] = "Iqbal"
+}
