8f81509b14809300ed2eab16782458923abeee46
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-30 09:37:46 +0700

    Update struct.go

--- day3/struct.go
+++ day3/struct.go
@@ -8,22 +8,29 @@ type Customer struct {
   
 }
 
+func (customer Customer) sayHi(name string) {
+fmt.Println("hi ", name, "my name is ", customer.Name)
+}
+
 func main(){
   var farhan Customer
   farhan.Name = "farhan"
   farhan.Address ="perum tti"
   farhan.Age = 22
 
-  fmt.Println(farhan)
-  fmt.Println(farhan.Nama)
-  fmt.Println(farhan.Address)
-  fmt.Println(farhan.Age)
+  farhan.sayHi("adya")
+  
+
+  // fmt.Println(farhan)
+  // fmt.Println(farhan.Nama)
+  // fmt.Println(farhan.Address)
+  // fmt.Println(farhan.Age)
  
   //another way to build struct
 
-  farhan := Customer {
-    Name : "Farhan",
-    Address : "Perum tti",
-    Age : 22,
-    }
+  // farhan := Customer {
+  //   Name : "Farhan",
+  //   Address : "Perum tti",
+  //   Age : 22,
+  //   }
 }
