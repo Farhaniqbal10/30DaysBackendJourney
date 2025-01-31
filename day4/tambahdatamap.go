cc0ad52f260d7f74f2908c7bc9e304e556a944d6
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-31 09:34:51 +0700

    Create tambahdatamap.go

---
+++ day4/tambahdatamap.go
@@ -0,0 +1,20 @@
+package main 
+
+import "fmt"
+
+func main {
+  //gunakan interface jika tipe data nya beda beda
+ var users []map[string]interface{}
+  for {
+    var nama string
+    var umur int
+    var email string
+
+    fmt.Println("masukan nama")
+    fmt.Scanln(&nama)
+
+    fmt.Println("masukan umur")
+    fmt.Scanln(&umur)
+    }
+  
+  }
