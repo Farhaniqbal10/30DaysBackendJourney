8308a3dc9875d8eb30ad0f5cd15bdcd926a13b8d
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-31 10:39:12 +0700

    Update searchbyID.go

--- day4/searchbyID.go
+++ day4/searchbyID.go
@@ -47,3 +47,23 @@ func main() {
     for _, user := range users {
         fmt.Printf("ID: %d | Nama: %s, Umur: %d, Email: %s\n", user.ID, user.Nama, user.Umur, user.Email)
     }
+
+    // Pencarian pengguna berdasarkan ID
+    var searchID int
+    fmt.Print("\nMasukkan ID pengguna yang ingin dicari: ")
+    fmt.Scanln(&searchID)
+
+    var found bool
+    for _, user := range users {
+        if user.ID == searchID {
+            fmt.Println("\nData Pengguna Ditemukan:")
+            fmt.Printf("ID: %d | Nama: %s, Umur: %d, Email: %s\n", user.ID, user.Nama, user.Umur, user.Email)
+            found = true
+            break
+        }
+    }
+
+    if !found {
+        fmt.Println("ID tidak ditemukan!")
+    }
+}
