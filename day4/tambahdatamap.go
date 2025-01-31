5f17fdfb24b747eb736b023ebbb73b8b9d2ed65a
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-31 09:39:19 +0700

    Update tambahdatamap.go

--- day4/tambahdatamap.go
+++ day4/tambahdatamap.go
@@ -15,6 +15,25 @@ func main {
 
     fmt.Println("masukan umur")
     fmt.Scanln(&umur)
+
+    fmt.Println("masukan email")
+    fmt.Scanln(&email)
+
+    user := map[string]interface{
+      "nama": nama,
+      "umur": umur,
+      "email" : email,
+      }
+
+    users = append(users, user)
+
+    var lagi string
+    fmt.Println("tambah data lagi ? (y/n) : ")
+    fmt.Scanln(&lagi)
+
+    if lagi != "y" {
+      break  
+      }
     }
   
   }
