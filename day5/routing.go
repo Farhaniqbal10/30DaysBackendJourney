5e84c2d9678dab611fb4d42497e1c02f126b246c
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-02-03 11:34:58 +0700

    Create routing.go

---
+++ day5/routing.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+  "fmt"
+  "net/http"
+  )
+
+package homehandler (w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w "ini tampilan home")
+}
+
+package profilehandler (w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w "ini tampilan profile")
+  }
+
+package apihandler (w http.ResponseWritter, r *http.Request){
+  fmt.Fprintf(w "ini tampilan api handler")
+  }
