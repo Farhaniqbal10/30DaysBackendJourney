91051be1b4858f624ea31ce5c7044044329ee119
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-02-04 16:01:52 +0700

    Update endpoint.go

--- day6/endpoint.go
+++ day6/endpoint.go
@@ -10,4 +10,10 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// Set header agar respons dalam format JSON
 	w.Header().Set("Content-Type", "application/json")
 
+	// Buat response dalam bentuk JSON
+	response := map[string]string{"message": "Hello, World!"}
+
+	// Encode response menjadi JSON dan kirim ke client
+	json.NewEncoder(w).Encode(response)
+
 }
