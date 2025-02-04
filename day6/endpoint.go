0c7c180347ff6e4e7fc569398c2f498f43a012fa
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-02-04 16:01:38 +0700

    Update endpoint.go

--- day6/endpoint.go
+++ day6/endpoint.go
@@ -4,3 +4,10 @@ import (
 	"encoding/json"
 	"net/http"
 )
+
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Set header agar respons dalam format JSON
+	w.Header().Set("Content-Type", "application/json")
+
+}
