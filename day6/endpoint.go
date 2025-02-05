07817c6bd8744d5b5ba010b1456d778c05065891
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-02-05 09:56:42 +0700

    Update endpoint.go

--- day6/endpoint.go
+++ day6/endpoint.go
@@ -36,7 +36,7 @@ func greetHandler(w http.ResponseWriter, r *http.Request) {
 
 
 func main() {
-	http.handleFunc("/greet/", greetHandler) 
+	http.HandleFunc("/greet/", greetHandler) 
 	http.HandleFunc("/hello", helloHandler) // Endpoint /hello
 	http.ListenAndServe(":3000", nil)       // Jalankan server di port 3000
 }
